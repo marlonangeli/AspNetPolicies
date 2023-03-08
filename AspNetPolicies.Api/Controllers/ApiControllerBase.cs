2266cf1424232662aa780b34b7797b952f240247
Marlon Angeli <marlonangeli91@gmail.com>
2023-03-08 16:14:43 -0300

    add project structure

---
+++ AspNetPolicies.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetPolicies.Api.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+    }
+}
