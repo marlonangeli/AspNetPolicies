82b77333f456a961414d8e6ba6754c11bc4078c6
Marlon Angeli <marlonangeli91@gmail.com>
2023-03-08 16:52:59 -0300

    add swagger and versioning

--- src/AspNetPolicies.Api/Controllers/v1/UserController.cs
+++ src/AspNetPolicies.Api/Controllers/v1/UserController.cs
@@ -4,7 +4,7 @@
 
 namespace AspNetPolicies.Api.Controllers.v1
 {
-    [("1.0")]
+    [ApiVersion("1.0")]
     public class UserController : ApiControllerBase
     {
         // GET: api/<UserController>
---
+++ src/AspNetPolicies.Api/Extensions/SwaggerSetup.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace AspNetPolicies.Api.Extensions;
+
+public static class SwaggerSetup
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "AspNetPolicies.Api",
+                Version = "v1"
+            });
+        });
+    }
+
+    public static void UseSwaggerUI(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            var apiVersionProvider = app.Services.GetService<IApiVersionDescriptionProvider>();
+            if (apiVersionProvider is null)
+                throw new ArgumentException("API Versioning not registered");
+
+            foreach (var description in apiVersionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName);
+            }
+            options.RoutePrefix = "swagger";
+
+            options.DocExpansion(DocExpansion.List);
+        });
+    }
+}
